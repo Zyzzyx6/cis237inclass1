1724e3f48a45429efcd5c54f456f8f4ac6ea63c3
Marty <wrusson0488@mail.kvcc.edu>
2016-09-13 16:50:18 -0400

    Added Employee Class

---
+++ cis237Inclass1/Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class Employee
+    {
+        //+++++++++++++++++++++++++
+        //Backing Fields
+        //+++++++++++++++++++++++++
+        private string _firstName;
+        private string _lastName;
+        private decimal _weeklySalary;
+
+        //+++++++++++++++++++++++
+        //Properties
+        //+++++++++++++++++++++++
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = value; }
+        }
+
+        //+++++++++++++++++++++
+        //Public Methods
+        //+++++++++++++++++++++
+
+        public override string ToString()
+        {
+            return this._firstName + " " + this._lastName;
+        }
+
+        public decimal YearlySalary()
+        {
+            return this._weeklySalary * 52;
+
+        }
+
+        //+++++++++++++++++++++
+        //Constructor
+        //+++++++++++++++++++++
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._weeklySalary = weeklySalary;
+
+        }
+
+
+        public Employee()
+        {
+            //Do Nothing
+        }
+
+
+
+    }
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,6 +10,38 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+
+            Employee myEmployee = new Employee();
+
+            myEmployee.FirstName = "Marty";
+            myEmployee.LastName = "Russon";
+            myEmployee.WeeklySalary = 2048.34m;
+
+            Console.WriteLine(myEmployee.FirstName);
+            Console.WriteLine(myEmployee);
+
+            Employee[] employees = new Employee[12];
+
+            employees[0] = new Employee("James", "Kirk", 453.00m);
+            employees[1] = new Employee("Jen-Luc", "Picard", 453.00m);
+            employees[2] = new Employee("Benjamin", "Sisko", 453.00m);
+            employees[3] = new Employee("Katheryn", "Janeway", 1253.00m);
+            employees[4] = new Employee("Jonathon", "Archer", 953.00m);
+            employees[5] = new Employee("Bob", "Kirk", 253.00m);
+            employees[6] = new Employee("Bill", "Kirk", 353.00m);
+            employees[7] = new Employee("Steve", "Kirk", 553.00m);
+            employees[8] = new Employee("Maggy", "Kirk", 653.00m);
+            employees[9] = new Employee("Wilbur", "Kirk", 753.00m);
+
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                { 
+                Console.WriteLine(employee.ToString() + " " + employee.YearlySalary());
+                }
+            }
+            
         }
     }
 }
